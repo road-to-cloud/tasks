39d0a611934a86851e77597f6a05a48f66f884d4
NicolasBissig <nicbissig@gmail.com>
2024-05-22 12:32:27 +0200

    fix: base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	base: '/tasks',
 	integrations: [
 		starlight({
 			title: 'Road to Cloud',
